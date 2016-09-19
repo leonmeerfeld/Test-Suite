770fd7c26416285c23bc18ff9611498bbec18dd2
leonmeerfeld <leon.meerfeld@gmail.com>
2016-09-19 10:09:46 +0200

    Added files

---
+++ ut-test-suite/ListViewInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ut_test_suite
+{
+    class ListViewInteraction
+    {
+        //private void InitializeListView()
+        //{
+            
+        //}
+
+        //public void AddSavedTestsToDashboard()
+        //{
+        //    string currentPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+        //    Directory.CreateDirectory(currentPath + "TestDir\\");
+
+        //    foreach(string pathToTest in Directory.GetFiles(currentPath + "TestDir\\"))
+        //    {
+        //        MainWindow mw = new MainWindow();
+        //        mw.AddToListView(pathToTest);
+        //    }
+        //}
+    }
+}
---
+++ ut-test-suite/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ut_test_suite
+{
+    /// <summary>
+    /// Object that describes tests for Databinding in the DasboardListView
+    /// </summary>
+    public class Tests
+    {
+        /// <summary>
+        /// Name of the test.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Determines whether the test is running.
+        /// </summary>
+        public string status { get; set; }
+
+        /// <summary>
+        /// Determines whether the test has been run.
+        /// </summary>
+        [DefaultValue(0)]
+        public int stage { get; set; }
+
+        /// <summary>
+        /// Determines the result of the test.
+        /// </summary>
+        public string result { get; set; }
+    }
+}
